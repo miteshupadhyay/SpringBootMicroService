658f9966788c63105ee768ceb20032a65b379ff2
Mitesh Upadhyay <upadhyaymitesh91.com>
2019-12-22 08:34:41 +0530

    first Commit

---
+++ src/test/java/com/miteshtech/rest/SampleRest/SampleRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miteshtech.rest.SampleRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
